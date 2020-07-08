7d4a84409b5133bbf2ee11e0c6e6a024a42e01b4
tebmik <lloyd.s.tom@gmail.com>
2020-07-08 10:54:48 +0100

    globalStyles parenting App and global styles

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import theme from "./components/utils/theme";
+import GlobalStyles from "./components/utils/global";
+import { ThemeProvider } from "styled-components";
+
 import App from "./App";
 
 ReactDOM.render(
-    <App />,
+    <ThemeProvider theme={theme}>
+        <>
+            <App />
+            <GlobalStyles />
+        </>
+    </ThemeProvider>,
     document.querySelector("#root")
 );
