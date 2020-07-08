548060597f67d8a012ffa6823415aa64a2b42651
tebmik <lloyd.s.tom@gmail.com>
2020-07-08 10:55:27 +0100

    imported Layout with child components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from "react";
+import Layout from "./components/layout/Layout";
+import Hero from "./containers/hero/Hero";
+import About from "./containers/about/About";
+import Gallery from "./containers/gallery/Gallery";
+import Contact from "./containers/contact/Contact";
+import Footer from "./containers/footer/Footer"
 
 const App = () => {
     return (
         <>
-            <p>App</p>
+            <div id="app">
+                <Layout>
+                    <Hero />
+                    <About />
+                    <Gallery />
+                    <Contact />
+                    <Footer />
+                </Layout>
+            </div>
         </>
     );
 };
