9c408ec5309dd8756270c1b032858891fd5dddf4
tebmik <lloyd.s.tom@gmail.com>
2020-07-08 10:51:22 +0100

    Component bare

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,20 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledNav = styled.div`
+    height: 8rem;
+    width: 100%;
+    background-color: gray;
+`;
+
+const Navbar = () => {
+    return (
+        <>
+            <StyledNav id="navbar">
+
+            </StyledNav>
+        </>
+    );
+};
+
+export default Navbar;
