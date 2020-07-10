122294cdd32c28d8e7dde1e9660e3a5f9255a138
tebmik <lloyd.s.tom@gmail.com>
2020-07-10 09:08:36 +0100

    More styling added and props passed to NavItems to handle color and weight

--- src/components/navbar/navContainer/mobileNav/drawer/MobileDrawer.js
+++ src/components/navbar/navContainer/mobileNav/drawer/MobileDrawer.js
@@ -5,14 +5,19 @@ import NavItems from "../../../navItems/NavItems";
 
 
 const StyledDrawer = styled.div`
+    border-top-right-radius: 2rem;
+    z-index: 1000;
     position: fixed;
+    top: 0;
     height: 100%;
     width: 70vw;
-    background-color: var(--color-text);
-    visibility: ${({opened}) => (opened ? "visible" : "none")};
+    background: gray;
+    visibility: ${({opened}) => (opened ? "visible" : "hidden")};
     transform: translateX(${({opened}) => (opened ? "0" : "-70vw")});
     opacity: ${({opened}) => (opened ? "1" : "0")};
     transition: all 0.2s cubic-bezier(0.65, 0, 0.35, 1);
+    box-shadow: ${(props) =>
+        props.opened ? "10px 0 5px -2px rgba(225,225,225,.1)" : null};
 
     @media only screen and (min-width: 769px) {
         display: none;
@@ -23,7 +28,7 @@ const MobileDrawer = ({ opened, clicked }) => {
     return (
         <>
             <StyledDrawer opened={opened} >
-                <NavItems mobile clicked={clicked} />
+                <NavItems white heavy mobile clicked={clicked} />
             </StyledDrawer>
         </>
     );
