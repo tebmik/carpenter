0b08442b24a53a945246058580bd8f84dbf22d2b
tebmik <lloyd.s.tom@gmail.com>
2020-07-10 09:04:55 +0100

    StyledWrapper component and a scrollNav

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,12 +1,40 @@
 import React from "react";
+import styled from "styled-components";
 import Container from "../../containers/Container";
 import NavContainer from "../navbar/navContainer/NavContainer";
+import OnScrollNav from "../navbar/onScrollNav/OnScrollNav";
+
+const StyledWrapper = styled.div`
+    display: flex;
+    justify-content: start;
+    z-index: 500;
+    min-height: 100vh;
+    position: relative;
+    margin: 0 auto;
+    transform: translateY(-5%);
+    background-color: var(--color-white);
+    width: 100%;
+    border-radius: 2rem;
+    max-width: 85vw;
+    box-shadow: 0 24px 38px 3px rgba(0,0,0,0.14), 0 9px 46px 8px rgba(0,0,0,0.12), 0 11px 15px -7px rgba(0,0,0,0.20);
+
+    @media only screen and (min-width: 769px) {
+        max-width: 140rem;
+        
+    }
+`;
+
 
 const Layout = ({ children }) => {
     return (
         <>
             <NavContainer />
-            <Container>{children}</Container>
+            <OnScrollNav />
+            <StyledWrapper id="bodyWrapper">
+                <Container>
+                    {children}
+                </Container>
+            </StyledWrapper>
         </>
     );
 };
