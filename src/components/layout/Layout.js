3df46e317aff454f23f857a8f66e094e59f351ea
tebmik <lloyd.s.tom@gmail.com>
2020-07-11 10:42:31 +0100

    Changed the max-width of laptop view and set overflow to hidden

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -17,9 +17,10 @@ const StyledWrapper = styled.div`
     border-radius: 2rem;
     max-width: 85vw;
     box-shadow: 0 24px 38px 3px rgba(0,0,0,0.14), 0 9px 46px 8px rgba(0,0,0,0.12), 0 11px 15px -7px rgba(0,0,0,0.20);
+    overflow: hidden;
 
-    @media only screen and (min-width: 769px) {
-        max-width: 140rem;
+    @media only screen and (min-width: 1025px) {
+        max-width: 120rem;
         
     }
 `;
