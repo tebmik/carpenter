28832afc60975cdaf0fd586b0a9adfde8f0a4bdd
tebmik <lloyd.s.tom@gmail.com>
2020-07-11 10:53:14 +0100

    removed styled P component and replaced with new Typography component with props

--- src/containers/styled/index.js
+++ src/containers/styled/index.js
@@ -19,28 +19,52 @@ export const H1 = styled.h1`
     )}
 `;
 
-export const H2 = styled.h2``;
-
-export const H3 = styled.h3``;
-
-export const P = styled.p`
-        font-size:${({title, subtitle, para}) => {
-            if(title) return "3.2rem";
-            if(subtitle) return "3rem";
-            if(para) return "1.6rem";
+export const Typography = styled.p`
+    font-size:${({size}) => {
+        if(size === "titleResponsive") return "2.4rem";
+        if(size === "subResponsive") return "2rem";
+        if(size === "title") return "3.2rem";
+        if(size === "sub") return "3rem";
+        if(size === "para") return "1.6rem";
+    }};
+    color: ${({color}) => {
+        if(color === "darker") return "var(--color-heading)";
+        if(color === "dark") return "var(--color-text)";
+        if(color === "white") return "var(--color-white)" 
+    }}; 
+    font-weight: ${({weight}) => {
+        if(weight === "700") return "700"
+        if(weight === "400") return "400";
+        if(weight === "100") return "100";
+    }};
+    font-style: ${({fontStyle}) => {
+        // if(fontStyle === "normal") return "normal";
+        if(fontStyle === "italic") return "italic";
+    }};
+    margin: ${({margin}) => {
+        if(margin === "margin") return "2rem 0rem";
+        if(margin === "marginTop") return "4rem 0 0 0";
+    }};
+    font-family: ${({font}) => {
+        if(font === "roboto") return "'Roboto', sans-serif";
+        if(font === "montserrat") return "'Montserrat', sans-serif";
+    }};
+    line-height: 1.6;
+    padding: 1rem;
+    @media only screen and (min-width: 769px) {
+        font-size:${({size}) => {
+            if(size === "titleResponsive") return "3vw";
+            if(size === "subResponsive") return "2.6vw";
         }};
-        color: var(--color-hading);
-        font-weight: ${({title, subtitle}) => {
-            if(title) return "400";
-            if(subtitle) return "thin";
+        padding: 0;
+    }
+    @media only screen and (min-width: 1441px) {
+        font-size:${({size}) => {
+            if(size === "titleResponsive") return "4.3rem";
+            if(size === "subResponsive") return "3.7rem";
         }};
-        font-style: ${({normal, italic}) => {
-            if(normal) return "normal";
-            if(italic) return "italic";
-        }};
-        margin: ${({margin}) => {
-            if(margin) return "2rem 0rem"
-        }};
-
-        font-family: "Roboto", sans-serif;
+        padding: 0;
+    }
+    
 `;
+ 
