11b8724d9203f17441c60ad512a2fe3b383894af
tebmik <lloyd.s.tom@gmail.com>
2020-07-11 10:46:11 +0100

    more styling for Mui themeProvider

--- src/components/navbar/navItems/item/NavItem.js
+++ src/components/navbar/navItems/item/NavItem.js
@@ -1,10 +1,12 @@
 import React from "react";
-import { makeStyles } from '@material-ui/core/styles';
+import styled from "styled-components";
+
+// Mui components 
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
-import styled from "styled-components";
+
 
 const StyledItem = styled(ListItem)`
     width: 100%;
@@ -12,7 +14,7 @@ const StyledItem = styled(ListItem)`
     justify-items: ${({mobile}) => (mobile ? "start" : "center")};
     
     cursor: pointer;
-    // padding: 1rem 2rem;
+
     color: ${({white, dark}) => {
         if(white) return "var(--color-white)";
         if(dark) return "var(--color-heading)";
@@ -24,37 +26,36 @@ const StyledItem = styled(ListItem)`
 `;
 
 const Item = styled(ListItemText)`
-    font-size: 1.6rem; !important
+
 `;
 
 
 const theme = createMuiTheme({
     typography: {
         // Some CSS
-        fontSize: "1.6rem",
+        fontSize: 23,
+        fontFamily: "'Montserrat',sans-serif"
     },
 });
 
 
 
 const NavItem = ({ title, mobile, clicked, white, dark, heavy, light }) => {
-    // const classes = useStyles();
     return (
         <>
             <ThemeProvider theme={theme}>
-            <List component="nav" aria-label="main menu">
-                <StyledItem
-                    button
-                    heavy={heavy}
-                    light={light} 
-                    white={white} 
-                    dark={dark} 
-                    mobile={mobile} 
-                    onClick={clicked}>
-                        {/* <CssBaseline /> */}
-                        <Item inset={true} primary={title} />
-                </StyledItem>
-            </List>
+                <List component="nav" aria-label="main menu">
+                    <StyledItem
+                        button
+                        heavy={heavy}
+                        light={light} 
+                        white={white} 
+                        dark={dark} 
+                        mobile={mobile} 
+                        onClick={clicked}>
+                            <Item inset={true} primary={title} />
+                    </StyledItem>
+                </List>
             </ThemeProvider>
    
         </>
