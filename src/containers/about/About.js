d1793a82ca83c0d1023cd4b962f48f03a27a77ff
tebmik <lloyd.s.tom@gmail.com>
2020-07-11 10:49:19 +0100

    added typography from reusable typography styled-component

--- src/containers/about/About.js
+++ src/containers/about/About.js
@@ -1,19 +1,20 @@
 import React from "react";
 import { makeStyles } from '@material-ui/core/styles';
 import styled from "styled-components";
-import { StyledPageWrapper } from "../styled";
+import { StyledPageWrapper, Typography } from "../styled";
 import ButtonBase from '@material-ui/core/ButtonBase';
+
+import Divide from "../../components/divider/DividerMargin";
 
 // call to actions
 import CallToAction from "../../components/buttons/Button";
 
 //from material ui
-import Divider from '@material-ui/core/Divider';
+// import Divider from '@material-ui/core/Divider';
 
-import { P } from "../styled";
 
 const StyledWrapper = styled.div`
-    min-height: 100vh;
+    min-height: 100%;
     width: 100%;
     position: relative;
     display: flex;
@@ -115,17 +116,18 @@ const About = () => {
         <>
         <StyledPageWrapper id="aboutWrapper">
             <StyledWrapper id="aboutContent">
-                <P title normal >{title.t2}</P>
-                <P subtitle italic >{title.subtitle.sub1}</P>
-                <Divider />
+                <Divide margin="big" />
+                <Typography size="title" font="roboto" weight="100" >{title.t2}</Typography>
+                <Typography size="sub" font="roboto" weight="100" fontStyle="italic" >{title.subtitle.sub1}</Typography>
+                <Divide margin="big" />
                 <MainContent>
                     <ParaWrapper>
-                        <P para margin>
+                        <Typography size="para" font="montserrat" >
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque facilisis justo vel ligula rutrum sollicitudin. Vivamus suscipit iaculis ligula. Donec facilisis ultricies imperdiet. Morbi convallis ante magna, eget pharetra nunc suscipit ut. Sed sed convallis ipsum. Etiam iaculis, est ac aliquam maximus, est ipsum condimentum lacus, eget commodo ante lorem ac orci. Vestibulum quis nulla porta, elementum turpis ut, rhoncus odio. 
-                        </P>   
-                        <P para margin>
+                        </Typography>   
+                        <Typography size="para" font="montserrat" margin="margin">
                             Cras ut gravida ipsum. Integer posuere lacus sed rutrum cursus. Phasellus aliquet ante ac vehicula posuere. Morbi dignissim lectus in velit luctus vehicula. Maecenas porta sapien dictum neque aliquet, ac gravida dolor auctor. Sed et convallis risus. Nam laoreet placerat dolor, eget elementum dolor facilisis eget
-                        </P>
+                        </Typography>
                     </ParaWrapper>
                     <CallToActionWrapper>
                         <ImageContainer>
