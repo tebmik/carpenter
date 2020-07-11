77fd14f7e6cefa6707de9c51f69ad784bddeea81
tebmik <lloyd.s.tom@gmail.com>
2020-07-11 10:48:10 +0100

    removed padding for mobile view and inseted to media query for larger device

--- src/containers/Container.js
+++ src/containers/Container.js
@@ -6,7 +6,10 @@ const Wrapper = styled.div`
     margin: 0 auto;
     max-width: 120rem;
     height: calc(100% - 8rem);
-    padding: 2rem;
+
+    @media only screen and (min-width: 769px) {
+        padding: 2rem;
+    }
 `
 
 const Container = ({ children }) => {
